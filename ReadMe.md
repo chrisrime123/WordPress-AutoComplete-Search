98229badf8794b247ac80ba066fbd526bdaef4de
Chris <chrisriminton@hotmail.co.uk>
2019-07-18 15:04:00 +0100

    Read Me Added

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# WordPress AutoComplete Search
+  - Searches posts and pages from a full screen overlay
+#
+#
+
+| File | Description |
+| ------ | ------ |
+| app.js | Javascript code - place in footer.php or JS file. |
+| functions.php | Add snippet from this file to functions.php |
+| header.php | Add this code after body tag |
+| styles.css | Edit and add to stylesheet |
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var typingTimer;
-var doneTypingInterval = 1000;  //time in ms, 5 second for example
+var doneTypingInterval = 1000; //time in ms, 5 second for example
 var $input = $('#find');
 
 //on keyup, start the countdown
 $input.on('keyup', function () {
-  clearTimeout(typingTimer);
-  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+	clearTimeout(typingTimer);
+	typingTimer = setTimeout(doneTyping, doneTypingInterval);
 });
 
 //on keydown, clear the countdown 
@@ -14,20 +14,20 @@ $input.on('keydown', function () {
 });
 
 //user is "finished typing," lets search
-function doneTyping () {
+function doneTyping() {
 	search();
 }
 
-function openSearch(){
+function openSearch() {
 	$('.search-overlay').toggleClass('search-overlay-show');
 	$('#find').focus();
 }
 
-function search(){
+function search() {
 	var spinner = "";
-	if(spinner != ''){
+	if (spinner != '') {
 		$('#results').html('').html('<img src="" style="width:50px;margin-bottom:15px;"/><br/><span style="text-align:center;">Searching...</span>');
-	}else{
+	} else {
 		$('#results').html('').html('<span style="text-align:center;">Searching...</span>');
 	}
 	jQuery.ajax({
@@ -37,23 +37,22 @@ function search(){
 			action: 'custom_search',
 			s: $('#find').val()
 		},
-		success : function( response ) {
+		success: function (response) {
 			$('#find').blur();
-			var obj = jQuery.parseJSON( response );
+			var obj = jQuery.parseJSON(response);
 			var i;
 			var txt = "";
-			if(obj.length){
-				for(i=0;i<obj.length; i++){
-					txt = txt + '<div class="large-12 columns" style="text-align:left;margin-bottom:15px; position: relative;" data-wow-duration="0.5s" data-wow-offset="900"><a href="'+obj[i].guid+'" target="_blank"><h5> '+ obj[i].post_title +'</h5><h6>'+ obj[i].post_content.substring(0, 100) +'</h6><br/><span style="position: absolute;top: 25%;right: 0;font-size: 20px;color: #d71f3a;"><i class="fas fa-chevron-right"></i></span></a></div>';
+			if (obj.length) {
+				for (i = 0; i < obj.length; i++) {
+					txt = txt + '<div class="large-12 columns" style="text-align:left;margin-bottom:15px; position: relative;" data-wow-duration="0.5s" data-wow-offset="900"><a href="' + obj[i].guid + '" target="_blank"><h5> ' + obj[i].post_title + '</h5><h6>' + obj[i].post_content.substring(0, 100) + '</h6><br/><span style="position: absolute;top: 25%;right: 0;font-size: 20px;color: #d71f3a;"><i class="fas fa-chevron-right"></i></span></a></div>';
 				}
-				setTimeout(function(){
+				setTimeout(function () {
 					$('#results').html('').append(txt);
 				}, (900));
-			}else{
+			} else {
 				txt = 'No results found!';
 				$('#results').html('').append(txt);
 			}
 		}
 	});
 }
-
